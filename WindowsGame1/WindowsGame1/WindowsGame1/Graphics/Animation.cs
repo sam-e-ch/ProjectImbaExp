a8d665aaae312d64a76a9764ac07fdd627066188
shaechler <babemusicstar@hotmail.com>
2012-06-06 12:20:32 +0200

    Initial commit

---
+++ WindowsGame1/WindowsGame1/WindowsGame1/Graphics/Animation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsGame1.Graphics
+{
+    class Animation
+    {
+        public Animation()
+        {
+        }
+
+    }
+}
